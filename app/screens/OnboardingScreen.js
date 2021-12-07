4de9c4794868fcf3d9d896e2dfc757c13f616b0e
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-12-06 23:19:38 -0800

    finished onboarding

--- app/screens/OnboardingScreen.js
+++ app/screens/OnboardingScreen.js
@@ -17,19 +17,19 @@ return (
     pages={[
       {
         backgroundColor: '#fff',
-        image: <Image source={require('../assets/neighbor.png')} style={styles.image}/>,
+        image: <Image source={require('../assets/match.png')} style={styles.image}/>,
         title: 'Request Help',
         subtitle: 'By filling out our short form, get matched with professional volunteers to help with any requests you may have',
       },
       {
         backgroundColor: '#fff',
-        image: <Image source={require('../assets/neighbor.png')} style={styles.image}/>,
+        image: <Image source={require('../assets/call.png')} style={styles.image}/>,
         title: 'Response in minutes',
         subtitle: 'Volunteers have 2 minutes to respond to your request so you will receive help ASAP',
       },
       {
         backgroundColor: '#fff',
-        image: <Image source={require('../assets/neighbor.png')} style={styles.image}/>,
+        image: <Image source={require('../assets/rate.png')} style={styles.image}/>,
         title: 'Rate your experience',
         subtitle: 'Help make this app a safe space by holding our volunteers accountable, after each incident you will have the option to rate your volunteer',
       },
